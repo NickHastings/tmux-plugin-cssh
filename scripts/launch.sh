4548c59686974fa7d08c934b33fabd64a63f663a
Nick Hastings <nicholaschastings@gmail.com>
2020-10-16 14:34:17 +0900

    Script name changed.

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -2,5 +2,5 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$CURRENT_DIR/tcssh.sh  --list
+$CURRENT_DIR/tcssh  --list
 tmux command-prompt -p "Select cluster:" "run-shell '$CURRENT_DIR/tcssh %% 2>&1'"
